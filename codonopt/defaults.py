359c51e63284f5020e25e924aa7613a779774f5c
Aneu7373 <aneu@USIFCPCPF5T2EGC.localdomain>
2026-02-17 15:21:54 -0800

    added defaults.py

---
+++ codonopt/defaults.py
@@ -0,0 +1,9 @@
+# codonopt/defaults.py
+
+DEFAULT_FORBIDDEN_MOTIFS = [
+    "AATAAA",  # poly(A) signal (RNA AAUAAA)
+]
+
+ALWAYS_FORBIDDEN_MOTIFS = [
+    # keep empty for now unless you decide some motifs are truly non-optional
+]
